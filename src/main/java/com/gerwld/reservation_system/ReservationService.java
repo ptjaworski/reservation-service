12987c236c5f61e12bc24973697d4aa1f546480c
PJaworski <pjaworski.dev@gmail.com>
2025-12-26 12:12:34 +0100

    Hashmap

--- src/main/java/com/gerwld/reservation_system/ReservationService.java
+++ src/main/java/com/gerwld/reservation_system/ReservationService.java
@@ -4,41 +4,52 @@ import org.springframework.stereotype.Service;
 
 import java.time.LocalDate;
 import java.util.List;
+import java.util.Map;
+import java.util.NoSuchElementException;
 
 @Service
 public class ReservationService {
 
+    private final Map<Long, Reservation> reservationMap = Map.of(
+            1L,
+            new Reservation(
+                    0L,
+                    100L,
+                    40L,
+                    LocalDate.now(),
+                    LocalDate.now().plusDays(5),
+                    ReservatiomStatus.APPROVED
+            ),
+            2L,
+            new Reservation(
+                    1L,
+                    90L,
+                    490L,
+                    LocalDate.now(),
+                    LocalDate.now().plusDays(5),
+                    ReservatiomStatus.APPROVED
+            ),
+            3L,
+            new Reservation(
+                    0L,
+                    100L,
+                    40L,
+                    LocalDate.now(),
+                    LocalDate.now().plusDays(5),
+                    ReservatiomStatus.APPROVED
+            )
+    );
+
     public Reservation getReservationById(
             Long id
     ) {
-        return new Reservation(
-                id,
-                100L,
-                40L,
-                LocalDate.now(),
-                LocalDate.now().plusDays(5),
-                ReservatiomStatus.APPROVED
-        );
+       if(!reservationMap.containsKey(id)) {
+           throw new NoSuchElementException("Not found reservation by id: " + id);
+       }
+       return reservationMap.get(id);
     }
 
     public List<Reservation> findAllReservations() {
-        return List.of(
-                new Reservation(
-                        0L,
-                        100L,
-                        40L,
-                        LocalDate.now(),
-                        LocalDate.now().plusDays(5),
-                        ReservatiomStatus.APPROVED
-                ),
-                new Reservation(
-                        1L,
-                        100L,
-                        40L,
-                        LocalDate.now(),
-                        LocalDate.now().plusDays(5),
-                        ReservatiomStatus.APPROVED
-                )
-        );
+        return reservationMap.values().stream().toList();
     }
 }
