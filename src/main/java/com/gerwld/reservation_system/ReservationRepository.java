0a42474d940224e52b3e43d6dfbcfaec4d19b1c4
PJaworski <pjaworski.dev@gmail.com>
2026-01-16 10:26:45 +0100

    Add info

--- src/main/java/com/gerwld/reservation_system/ReservationRepository.java
+++ src/main/java/com/gerwld/reservation_system/ReservationRepository.java
@@ -1,7 +1,8 @@
 package com.gerwld.reservation_system;
 
 import org.springframework.data.jpa.repository.JpaRepository;
-
+// Jpa repository - интерфейс из data jpa, дает много методов для взаимодействия с БД
+//                                                          <Дженерик тип данных, Тип ID>
 public interface ReservationRepository extends JpaRepository<ReservationEntity, Long> {
 
 }
